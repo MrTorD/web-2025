201984aa66d7577fdaeb1c0d39039b9b98a33351
Kostantin <62954231+MrTorD@users.noreply.github.com>
2025-05-11 23:50:19 +0300

    Add files via upload

---
+++ RGR/RGR2/CountWords2.pas
@@ -0,0 +1,59 @@
+PROGRAM WordsList(INPUT, OUTPUT);
+USES
+  WordTypeUnit, 
+  NodeWork;
+
+CONST
+  NodesMaxCount = 100000;        
+
+PROCEDURE CountWords(VAR FIn: TEXT; VAR FOut: TEXT);
+VAR
+  Ch: CHAR;  
+  Word: WordType; 
+  F, TreeF, TempF: TEXT;
+  Root: Tree;
+  NodesCounter: INTEGER;     
+BEGIN {CountWords}
+  NodesCounter := 0;
+  Root := NIL;
+  REWRITE(F);
+  WHILE NOT EOF(FIn)
+  DO
+    BEGIN
+      WHILE NOT EOLN(FIn)
+      DO
+        IF FIn^ IN SupportableChars
+        THEN
+          BEGIN
+            ReadWord(FIn, Word);
+            Insert(Root, Word);
+            NodesCounter := NodesCounter + 1;
+            IF NodesCounter > NodesMaxCount
+            THEN
+              BEGIN
+                RESET(F); 
+                REWRITE(TempF); 
+                REWRITE(TreeF);
+                PrintTree(TreeF, Root);            
+                Merge(TempF, F, TreeF);
+                CopyFile(TempF, F);
+                DisposeMemory(Root);
+                Root := NIL;
+                NodesCounter := 0 
+              END 
+          END
+        ELSE
+          READ(FIn, Ch);
+      READLN(FIn)    
+    END;
+  REWRITE(TreeF);  
+  PrintTree(TreeF, Root);
+  DisposeMemory(Root);
+  Merge(TempF, F, TreeF);
+  CopyFile(TempF, F);       
+  CopyFile(F, FOut) 
+END; {CountWords}      
+
+BEGIN
+  CountWords(INPUT, OUTPUT)
+END. 
